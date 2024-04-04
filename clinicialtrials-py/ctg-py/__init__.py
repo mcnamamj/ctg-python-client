e0640797a586eaf5822e8364ef38fb08afddaa3b
Matt McNamara <matt@mattmcnamara.com>
2024-04-03 19:55:30 -0700

    Remove

--- clinicialtrials-py/ctg-py/__init__.py
+++
@@ -1,8 +0,0 @@
-"""A client library for accessing ClinicalTrials.gov REST API"""
-
-from .client import AuthenticatedClient, Client
-
-__all__ = (
-    "AuthenticatedClient",
-    "Client",
-)
--- clinicialtrials-py/ctg-py/models/analysis_dispersion_type.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-
-class AnalysisDispersionType(str, Enum):
-    STANDARD_DEVIATION = "STANDARD_DEVIATION"
-    STANDARD_ERROR_OF_MEAN = "STANDARD_ERROR_OF_MEAN"
-
-    def __str__(self) -> str:
-        return str(self.value)
